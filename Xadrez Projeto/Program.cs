d2a0db6c361432f1a58ac9e8eed4dec3523a2c1c
Matheus de Aquino <matheusaquinof@gmail.com>
2018-09-26 20:50:22 -0300

    Começando a implementar o tabuleiro

--- Xadrez Projeto/Program.cs
+++ Xadrez Projeto/Program.cs
@@ -5,11 +5,7 @@ namespace Xadrez_Projeto {
     class Program {
         static void Main(string[] args) {
 
-            Posicao P;
-
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
             Console.ReadLine();
         }
