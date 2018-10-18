e805304e3208a85e7feb5fa835f30510faa9d171
Matheus de Aquino <matheusaquinof@gmail.com>
2018-10-18 17:01:02 -0300

    implementando as restricoes de movimento

--- Xadrez Projeto/Xadrez/PartidaXadrez.cs
+++ Xadrez Projeto/Xadrez/PartidaXadrez.cs
@@ -5,8 +5,8 @@ namespace Xadrez {
     class PartidaXadrez {
 
         public Tabuleiro tab { get; private set; }
-        private int turno;
-        private Cor jogadorAtual;
+        public int turno { get; private set; }
+        public Cor jogadorAtual { get; private set; }
         public bool terminada { get; private set; }
 
         public PartidaXadrez() {
@@ -15,6 +15,39 @@ namespace Xadrez {
             jogadorAtual = Cor.Branca;
             terminada = false;
             colocarPecas();
+        }
+
+        public void realizaJogada(Posicao origem, Posicao destino) { //novo e anotar
+            executaMovimento(origem, destino);
+            turno++;
+            mudaJogador();
+        }
+
+        public void validarPosicaoDeOrigem(Posicao pos) {
+            if(tab.peca(pos) == null) {
+                throw new TabuleiroException("Não existe peca na posicao escolhida!");
+            }
+            if (jogadorAtual != tab.peca(pos).cor) {
+                throw new TabuleiroException("A peca de origem escolhida não é sua!");
+            }
+            if(!tab.peca(pos).existeMovimentosPossiveis()) {
+                throw new TabuleiroException("Não há movimentos possiveis para peca de origem escolhida!");
+            }
+        }
+
+        public void validarPosicaoDeDestino(Posicao origem, Posicao destino) {
+            if(!tab.peca(origem).podeMoverPara(destino)) {
+                throw new TabuleiroException("Posicao de destino invalida!");
+            }
+        }
+
+        private void mudaJogador() {
+            if(jogadorAtual == Cor.Branca) {
+                jogadorAtual = Cor.Preta;
+            }
+            else {
+                jogadorAtual = Cor.Branca;
+            }
         }
 
         public void executaMovimento(Posicao origem, Posicao destino) {
