0f56c44b63d7c0d7f71a29dfcfae116e01c3206c
Matheus de Aquino <matheusaquinof@gmail.com>
2018-10-21 16:01:10 -0300

    Controlando as peças capturadas

--- Xadrez Projeto/Program.cs
+++ Xadrez Projeto/Program.cs
@@ -17,10 +17,7 @@ namespace Xadrez_Projeto {
 
                     try {
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Tunro: " + partida.turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Tela.imprimirPartida(partida);
 
                         Console.WriteLine();
                         Console.Write("Origem:");
