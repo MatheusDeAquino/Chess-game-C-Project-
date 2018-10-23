7d77997a232119d3c5b08270ee759d2b05f0edfd
Matheus de Aquino <matheusaquinof@gmail.com>
2018-10-23 12:21:58 -0300

    Acrescentando todas as peças e posições corretas

---
+++ Xadrez Projeto/Xadrez/Cavalo.cs
@@ -0,0 +1,68 @@
+﻿using tabuleiro;
+
+namespace Xadrez {
+
+    class Cavalo : Peca {
+
+        public Cavalo(Tabuleiro tab, Cor cor) : base(tab, cor) {
+        }
+
+        public override string ToString() {
+            return "C";
+        }
+
+        private bool podeMover(Posicao pos) {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != this.cor;
+        }
+
+        public override bool[,] movimentosPossiveis() {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //esquerda esquerda cima
+            pos.definirValores(posicao.linha - 1, posicao.coluna - 2);
+            if (tab.posicaoValida(pos) && podeMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            //esquerda cima cima
+            pos.definirValores(posicao.linha - 2, posicao.coluna - 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            //direita  cima cima
+            pos.definirValores(posicao.linha - 2, posicao.coluna + 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            //direita direita cima
+            pos.definirValores(posicao.linha - 1, posicao.coluna + 2);
+            if (tab.posicaoValida(pos) && podeMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            //esquerda esquerda baixo
+            pos.definirValores(posicao.linha + 1, posicao.coluna - 2);
+            if (tab.posicaoValida(pos) && podeMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            //esqueda baixo baixo
+            pos.definirValores(posicao.linha + 2, posicao.coluna - 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            //direita baixo baixo
+            pos.definirValores(posicao.linha + 2, posicao.coluna + 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            //direita direita baixo
+            pos.definirValores(posicao.linha + 1, posicao.coluna + 2);
+            if (tab.posicaoValida(pos) && podeMover(pos)) {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            return mat;
+        }
+    }
+}
---
+++ Xadrez Projeto/Xadrez/Peão.cs
@@ -0,0 +1,76 @@
+﻿using tabuleiro;
+
+namespace Xadrez {
+
+    class Peao : Peca {
+
+        public Peao(Tabuleiro tab, Cor cor) : base(tab, cor) {
+        }
+
+        public override string ToString() {
+            return "P";
+        }
+
+        private bool existeInimigo(Posicao pos) {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != this.cor;
+        }
+
+        public bool livre(Posicao pos) {
+            return tab.peca(pos) == null;
+        }
+
+        public override bool[,] movimentosPossiveis() {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            if(cor == Cor.Branca) {
+                //frente uma casa
+                pos.definirValores(posicao.linha - 1, posicao.coluna);
+                if(tab.posicaoValida(pos) && livre(pos)) {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                //frente 2 casas
+                pos.definirValores(posicao.linha - 2, posicao.coluna);
+                if (tab.posicaoValida(pos) && livre(pos) && qteMovimentos == 0) {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                //comer peça diagonal esquerda
+                pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
+                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                //ccomer peça diagonal direita
+                pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
+                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+             }
+            //peao de cor preta tem direção invertida por causa daposicao do tabuleiro
+            else {
+                //frente uma casa
+                pos.definirValores(posicao.linha + 1, posicao.coluna);
+                if (tab.posicaoValida(pos) && livre(pos)) {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                //frente 2 casas
+                pos.definirValores(posicao.linha + 2, posicao.coluna);
+                if (tab.posicaoValida(pos) && livre(pos) && qteMovimentos == 0) {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                //comer peça diagonal esquerda
+                pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
+                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+                //ccomer peça diagonal direita
+                pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+                if (tab.posicaoValida(pos) && existeInimigo(pos)) {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+            }
+            return mat;
+        }
+    }
+}
